d26abb404f5ab65264bde50b861c435d7dc1539f
renacto-qa <renacto.qa@gmail.com>
2026-07-06 17:01:36 -0300

    Implementada leitura do código do memorial

--- js/memorial.js
+++ js/memorial.js
@@ -2,10 +2,46 @@
 // Memorial Peternizando
 // ==========================================
 
+// Inicia a aplicação
 iniciar();
 
-function iniciar() {
+/**
+ * Função principal da aplicação.
+ */
+async function iniciar() {
 
-    console.log("Aplicação iniciada.");
+    console.clear();
+
+    console.log("================================");
+    console.log(" Memorial Peternizando");
+    console.log("================================");
+
+    const codigo = obterCodigoMemorial();
+
+    console.log("Código recebido:", codigo);
+
+}
+
+/**
+ * Obtém código do memorial informado na URL.
+ *
+ * Exemplo:
+ * memorial.html?id=MP-TESTE001
+ */
+function obterCodigoMemorial() {
+
+    const parametros = new URLSearchParams(window.location.search);
+
+    const codigo = parametros.get("id");
+
+    if (!codigo) {
+
+        alert("Nenhum memorial informado.");
+
+        throw new Error("Parâmetro 'id' não encontrado.");
+
+    }
+
+    return codigo;
 
 }
